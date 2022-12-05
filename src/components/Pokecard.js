b0d77e37cd19edbff61e1fe00823405e16554327
Marnin Audu <msmaudu@gmail.com>
2022-12-05 03:22:26 +0100

    Fixed variable and constant declarations

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Pokecard = (props) => {
-  IMG_URL =
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png";
+  const IMG_URL =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
   let Img_RC = `${IMG_URL}${props.id}.png`;
   return (
     <div className="pokecard">
       <h1>{props.name}</h1>
-      <img src="Img_RC" alt="Image of pokemon" />
+      <img src={Img_RC} alt="A pokemon" />
     </div>
   );
 };
