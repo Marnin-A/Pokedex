fb14ff60d5085671d53336806a71e960d59e9124
Marnin Audu <msmaudu@gmail.com>
2022-12-08 14:05:02 +0100

    Removed cancel function which prevented multiple requests, added reminder for next step

--- src/App.js
+++ src/App.js
@@ -5,22 +5,15 @@ function App() {
   // Set pokemon and Loading states
   const [pokemon, setPokemon] = useState([]);
   const [Loading, setLoading] = useState(true);
-  // Create empty variable to be use to cancel axios requests
-  let cancel;
 
   // Set useEffect with axios to get API data
   useEffect(() => {
     setLoading(true); // Set initial loading state
-
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon/1", {
-        cancelToken: new axios.cancelToken((c) => (cancel = c)),
-      })
-      .then((res) => {
-        setLoading(false); // Set loading state to false after promise is fulfilled
-        const pokeData = res.data;
-        setPokemon(pokeData); // Set pokemon state to contain response data
-      });
+    axios.get("https://pokeapi.co/api/v2/pokemon/1").then((res) => {
+      setLoading(false); // Set loading state to false after promise is fulfilled
+      const pokeData = res.data;
+      setPokemon(pokeData); // Set pokemon state to contain response data
+    });
   }, []);
 
   // Set condition for loading state
@@ -32,7 +25,7 @@ function App() {
   return (
     <div>
       <Pokecard pokemon={pokemon} />
-      {/* Continue with app structure from React course */}
+      {/*🟡Next Step: Continue with app structure from React course 🟡*/}
     </div>
   );
 }
