fde0ed1a6539c9b3e7e6d60265c2443ffcdb0cf1
Marnin Audu <msmaudu@gmail.com>
2022-12-15 07:10:55 +0100

    Moved page counter to pokedex component, and updated page styling

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import Pokedex from "./components/Pokedex/Pokedex";
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // Page counter
-  // Set page counter state
-  const [counter, setCounter] = useState(1);
-  //increase counter
-  const increase = () => {
-    setCounter((count) => count + 1);
-  };
-
-  //decrease counter
-  const decrease = () => {
-    if (counter === 1) {
-      setCounter(1);
-    } else {
-      setCounter((counter) => counter - 1);
-    }
-  };
   // Return the component to be rendered
   return (
     <div className="App">
       <Pokedex />
-      {/* Page counter */}
-      <div className="counter">
-        <div className="btn__container">
-          <button className="control__btn" onClick={decrease}>
-            Prev
-          </button>
-          <span className="counter__output">{counter}</span>
-          <button className="control__btn" onClick={increase}>
-            Next
-          </button>
-        </div>
-      </div>
-      {/*🟡Next Step: Center page counter, and attach previous and next functions🟡*/}
+      {/*🟡Next features: 1. Attach previous and next functions🟡
+                          2. Add pokemon loading screen*/}
     </div>
   );
 }
--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Pokecard from "../Pokecard/Pokecard";
 import axios from "axios";
+import "./Pokedex.css";
 const Pokedex = () => {
   const [pokemon, setPokemon] = useState([]);
   // Set pokemon and Loading states
   const [Loading, setLoading] = useState(true);
+
+  // Set page counter state
+  const [counter, setCounter] = useState(1);
 
   // Set useEffect with axios to get API data
   useEffect(() => {
@@ -24,12 +28,42 @@ const Pokedex = () => {
     return "Loading...";
   }
 
+  // Page counter
+
+  //increase counter
+  const increase = () => {
+    setCounter((count) => count + 1);
+  };
+
+  //decrease counter
+  const decrease = () => {
+    if (counter === 1) {
+      setCounter(1);
+    } else {
+      setCounter((counter) => counter - 1);
+    }
+  };
+
   return (
     <div className="Pokedex">
-      <h1>Pokemon</h1>
+      <div className="title">
+        <h1>Pokemon</h1>
+      </div>
       {pokemon.map((p) => (
         <Pokecard url={p.url} name={p.name} />
       ))}
+      {/* Page counter */}
+      <div className="counter">
+        <div className="btn__container">
+          <button className="control__btn" onClick={decrease}>
+            Prev
+          </button>
+          <span className="counter__output">{counter}</span>
+          <button className="control__btn" onClick={increase}>
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
